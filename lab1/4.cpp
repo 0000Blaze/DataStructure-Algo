91482912822fa6b96af990724c18659cd988d9e4
RohanChhetry <47933141+0000Blaze@users.noreply.github.com>
2020-09-10 12:55:44 +0545

    lab 1 no.4 done

--- lab1/4.cpp
+++ lab1/4.cpp
@@ -1,9 +1,41 @@
 //Check the paired parenthesis in mathematical expression
 
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+//bracket check function
+void checkBrackets(string s){
+    int l =s.length();
+    stack<char> st;
+    st.push('N');
+    string final;
+//iteration through expression
+    for(int var=0;var<l;var++){
+    //starting bracket "("
+        if(s[var]=='(')
+            st.push('(');
+    //ending bracket ")" encountered first
+        else if(s[var]==')' && st.top()=='N'){
+            st.push(')');
+            cout<<"Error ) bracket before ( , ";
+            break;
+        }
+    //")" encountered after "("
+        else if(s[var]==')' && st.top()!='N'){
+            st.pop();
+        }
+    }
+//stack top is "N"
+    if(st.top()=='N')
+        cout<<"All brackets are paired";
+    else
+        cout<<"All brackets are not paired";
+}
 
+int main(){
+//expression to be checked
+//***************HARDCODED********************//
+    string exp = "(a+b)*(k+(s/e))";
+    checkBrackets(exp);
 return 0;
 }
