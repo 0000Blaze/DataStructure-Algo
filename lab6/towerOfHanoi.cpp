2c8d943ae90b8bf0f6c5931d63094c9a699fb969
RohanChhetry <47933141+0000Blaze@users.noreply.github.com>
2020-10-14 10:16:36 +0545

    Tower of hanoi completed

--- lab6/towerOfHanoi.cpp
+++ lab6/towerOfHanoi.cpp
@@ -6,7 +6,26 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main(){
+void TOH(int n,char Sour, char Aux,char Des)
+{
+	if(n==1)
+	{
+		cout<<"Move Disk "<<n<<" from "<<Sour<<" to "<<Des<<endl;
+		return;
+	}
 
-return 0;
+	TOH(n-1,Sour,Des,Aux);
+	cout<<"Move Disk "<<n<<" from "<<Sour<<" to "<<Des<<endl;
+	TOH(n-1,Aux,Sour,Des);
+}
+
+int main()
+{
+	int n;
+
+	cout<<"Enter no. of disks:";
+	cin>>n;
+	TOH(n,'A','B','C');
+
+	return 0;
 }
