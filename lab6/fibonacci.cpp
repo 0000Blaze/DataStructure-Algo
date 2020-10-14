824db0fd699cbea23936d2974199b5d779f0198d
RohanChhetry <47933141+0000Blaze@users.noreply.github.com>
2020-10-14 08:36:15 +0545

    fibonacci done

--- lab6/fibonacci.cpp
+++ lab6/fibonacci.cpp
@@ -6,11 +6,35 @@
 using namespace std;
 
 class Fibonacci{
-
-
+    int n,a,b,c,count=1;
+public:
+    int fibo(int , int);
+    void display();
 };
 
-int main(){
+int Fibonacci::fibo(int n1 , int n2){
+    if (count< n){
+        cout<<" "<<n1;
+        c=n1;
+        count++;
+        fibo(n1+n2,c);
+    }
+    else{
+        cout<<endl;
+        exit(EXIT_SUCCESS);
+    }
+}
 
+void Fibonacci::display(){
+    cout<<"Enter till what term fibonacci series required: ";
+    cin>>n;
+    a=0;b=1;
+    cout<<endl<<a;
+    fibo(b,a);
+}
+
+int main(){
+    Fibonacci f;
+    f.display();
 return 0;
 }
